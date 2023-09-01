dbd7b0d44e77d85c7b3e4e9d35a382052ca60671
Michal.Tkaczyk <michaltkaczyk@outlook.com>
2023-09-01 13:47:26 +0200

    quiting the fame

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import pygame
 from checkers.constants import WIDTH, HEIGHT
 
+FPS = 60
+
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Checkers")
 
 def main():
     run = True
+    clock = pygame.time.Clock()
     
     while run:
-        pass
+        clock.tick(FPS)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
 
 main()
