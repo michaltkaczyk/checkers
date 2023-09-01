2b2bbc3486b8ceb81f0c556311a13c2b2086add2
Michal.Tkaczyk <michaltkaczyk@outlook.com>
2023-09-01 14:09:29 +0200

    beige colors

--- checkers/constants.py
+++ checkers/constants.py
@@ -4,7 +4,7 @@ WIDTH, HEIGHT = 800, 800
 ROWS, COLS = 8, 8
 SQUARE_SIZE = WIDTH // COLS # Is this enough? What if WIDTH and HEIGHT are not equal?
 
-RED = (255, 0, 0)
+RED = (246, 230, 191)
 WHITE = (255, 255, 255)
-BLACK = (0, 0)
+BLACK = (102, 68, 58)
 BLUE = (0, 0, 255)
